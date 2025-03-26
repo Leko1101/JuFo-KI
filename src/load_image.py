a8c0f5ff691860db9d9328ebbf76534ccc166f68
Leko1101 <heidenreich.leondd@gmail.com>
2025-03-26 14:50:43 +0100

    removed unnecessary files

--- src/load_image.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-
-image = np.asarray(Image.open("drache.png").convert("L")) / 255.0
-
-print(image)
-plt.imshow(image)
-plt.show()
