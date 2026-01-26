bbf7bdc9edba80a6a7599de23b728edf211d33de
Leko1101 <heidenreich.leondd@gmail.com>
2026-01-26 19:24:14 +0100

    added framework for augmentation

--- augmented_agricultural_plant_dataset/augment.py
+++ augmented_agricultural_plant_dataset/augment.py
@@ -1,16 +1,37 @@
-import pillow
+from PIL import Image
 import random
-import src_images
+from plant_species import plant_species_map
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 random.seed(10)
 
+number_plant_species = 23
 number_augmented_imgs = 100
+DESTINATION_PATH = "./dest_imgs"
 
-create_augmented_img()
+def create_augmented_img():
+    ext = ".jpg"
+    ground_img = randint(0,10)
+    number_plants_in_img = randint(1,25)
     
+    for i in range(number_plants_in_img):
 
-if __name__ == "main" 
-    for i in range number_augmented_imgs
+        plant_species = randint(1,23)
+        species_name = plant_species_map[plant_species]
+        plant_specimen = randint(0,10)
+        
+        background = Image.open(f"./src_imgs/ground/{ground_img}{ext}").convert("RGBA")
+        foreground = Image.open(f"./src_imgs/{species_name}/{plant_specimen}{ext}").convert("RGBA")
+
+        background.paste(foreground, (0,0), foreground)
+        #background.show()
+        
+        
+
+
+if __name__ == "main":
+    for i in range(number_augmented_imgs):
         create_augmented_img()
 
 
