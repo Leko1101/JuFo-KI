6a4ed8913f4773f716cbf8337b429e4b7d36f3b0
Tom Heidenreich <53293648+tom-heidenreich@users.noreply.github.com>
2026-02-19 15:14:09 +0000

    changed file type to png

--- augmented_agricultural_plant_dataset/augment.py
+++ augmented_agricultural_plant_dataset/augment.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import random
 from plant_species import plant_species_map
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
 
 random.seed(10)
 
@@ -11,29 +11,29 @@ number_augmented_imgs = 100
 DESTINATION_PATH = "./dest_imgs"
 
 def create_augmented_img():
-    ext = ".jpg"
-    ground_img = randint(0,10)
-    number_plants_in_img = randint(1,25)
+    ext = ".png"
+    ground_img = random.randint(0,9)
+    number_plants_in_img = random.randint(1,25)
     
     for i in range(number_plants_in_img):
 
-        plant_species = randint(1,23)
+        plant_species = random.randint(1,23)
         species_name = plant_species_map[plant_species]
-        plant_specimen = randint(0,10)
+        plant_specimen = random.randint(0,9)
         
         background = Image.open(f"./src_imgs/ground/{ground_img}{ext}").convert("RGBA")
         foreground = Image.open(f"./src_imgs/{species_name}/{plant_specimen}{ext}").convert("RGBA")
 
+    
+
         background.paste(foreground, (0,0), foreground)
-        #background.show()
+        background.save("test.png")
         
         
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     for i in range(number_augmented_imgs):
         create_augmented_img()
-
-
 
 
--- augmented_agricultural_plant_dataset/convert.py
+++ augmented_agricultural_plant_dataset/convert.py
@@ -1,13 +1,34 @@
 import os
+from PIL import Image
+from plant_species import plant_species_map
 
 BASE_PATH = "./src_imgs"
 
 for root, dirs, files in os.walk(BASE_PATH):
     files = sorted([f for f in files if not f.startswith('.')])  # Skip hidden files
     for idx, filename in enumerate(files):
-        ext = ".jpg"
+        ext = ".png"
         new_name = f"{idx}{ext}"
         old_path = os.path.join(root, filename)
         new_path = os.path.join(root, new_name)
         if old_path != new_path:
             os.rename(old_path, new_path)
+
+number_plant_species = 23
+ext = ".png"
+
+for i in range(number_plant_species+1):
+    species_name = plant_species_map[i]
+    for i in range(10):
+        img_path = f"./src_imgs/{species_name}/{i}{ext}"
+        img = Image.open(img_path)
+        if species_name == "ground":
+            img = img.resize([600,600]) 
+        else:
+            img = img.resize([100,100])
+        img.save(img_path)
+        
+
+
+
+
